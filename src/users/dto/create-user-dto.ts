bbc5254e8f221e2333ee5af9405645a34717c06c
Arthur Saito <arthur_saito1998@hotmail.com>
2023-07-14 20:24:27 -0300

    chore: adding users resource

---
+++ src/users/dto/create-user-dto.ts
@@ -0,0 +1,25 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class createUserDTO {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsStrongPassword({
+    minLength: 6,
+    minUppercase: 1,
+    minNumbers: 1,
+  })
+  password: string;
+}
