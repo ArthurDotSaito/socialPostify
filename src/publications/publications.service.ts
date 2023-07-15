101638b16a1e6700039b6cc0719baad2173f9767
Arthur Saito <arthur_saito1998@hotmail.com>
2023-07-15 18:41:30 -0300

    feat: implementing createPublication service

--- src/publications/publications.service.ts
+++ src/publications/publications.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PublicationsRepository } from './repository/publications.repository';
+import { CreatePublicationDTO } from './dto/create-publication.dto';
 
 @Injectable()
-export class PublicationsService {}
+export class PublicationsService {
+  constructor(private readonly publicationRepository: PublicationsRepository) {}
+
+  async createPublication(
+    data: CreatePublicationDTO,
+    userId: string,
+  ): Promise<void> {
+    const postExist = await this.publicationRepository.getPublicationWithTitle(
+      data.title,
+    );
+    if (postExist)
+      throw new HttpException('Post title already exist', HttpStatus.CONFLICT);
+
+    await this.publicationRepository.createPublication(data, userId);
+  }
+}
