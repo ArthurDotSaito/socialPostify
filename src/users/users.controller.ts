5e5d5e4726fe972bc3f9fad660a90c784c4e48e4
Arthur Saito <arthur_saito1998@hotmail.com>
2023-07-15 13:00:14 -0300

    feat: creating createUser Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDTO } from './dto/create-user-dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  createUSer(@Body() body: CreateUserDTO) {
+    return this.usersService.createUser(body);
+  }
 }
