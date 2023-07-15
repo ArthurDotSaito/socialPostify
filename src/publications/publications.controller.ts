164d8eb68fee22067fdfe582528eed93daff1927
Arthur Saito <arthur_saito1998@hotmail.com>
2023-07-15 18:52:05 -0300

    feat: adding publication controller

--- src/publications/publications.controller.ts
+++ src/publications/publications.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { PublicationsService } from './publications.service';
+import { CreatePublicationDTO } from './dto/create-publication.dto';
 
-@Controller('publications')
+interface RequestWithUserId extends Request {
+  userId: string;
+}
+
+@Controller()
 export class PublicationsController {
   constructor(private readonly publicationsService: PublicationsService) {}
+
+  @Post('publication')
+  async createPublication(
+    @Body() body: CreatePublicationDTO,
+    @Req() request: RequestWithUserId,
+  ) {
+    return await this.publicationsService.createPublication(
+      body,
+      request.userId,
+    );
+  }
 }
--- src/publications/publications.module.ts
+++ src/publications/publications.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PublicationsService } from './publications.service';
 import { PublicationsController } from './publications.controller';
+import { PublicationsRepository } from './repository/publications.repository';
+import { PublicationsPrismaRepository } from './repository/implementations/prisma-publication.repository';
 
 @Module({
   controllers: [PublicationsController],
-  providers: [PublicationsService]
+  providers: [
+    PublicationsService,
+    {
+      provide: PublicationsRepository,
+      useClass: PublicationsPrismaRepository,
+    },
+  ],
 })
 export class PublicationsModule {}
