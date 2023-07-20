2b625892f22834c5b594d5cb0666f787c3e3fed6
Arthur Saito <arthur_saito1998@hotmail.com>
2023-07-20 01:21:31 -0300

    chore: adding cleanDb function on prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,10 +1,26 @@
 import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(config: ConfigService) {
+    super({
+      datasources: {
+        db: { url: config.get<string>('DATABASE_URL') },
+      },
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async cleanDatabase() {
+    if (process.env.NODE_ENV === 'production') return;
+    const models = Reflect.ownKeys(this).filter((key) => key[0] !== '_');
+
+    return Promise.all(models.map((modelKey) => this[modelKey].deleteMany()));
   }
 
   async enableShutdownHooks(app: INestApplication) {
